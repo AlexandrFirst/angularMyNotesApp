6a257ce52681dd8c6a12d5f4eb2d4e41a8439324
Olexandr Lohvinov <ato31ato@gmail.com>
2021-07-10 22:25:57 +0300

    Notes markup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule

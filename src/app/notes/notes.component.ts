641334b21d85e9d43570cc9f407c7de6fbc3e1f6
Olexandr Lohvinov <ato31ato@gmail.com>
2021-07-12 22:44:07 +0300

    menu markup added

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
+
+  isSidebarActive = false;
+
+  toggleSidebar(){
+    this.isSidebarActive = !this.isSidebarActive;
+  }
+
   constructor() { }
 
   ngOnInit(): void {

9b06374c38c6778017217f9ce1340f9f005f4661
Olexandr Lohvinov <ato31ato@gmail.com>
2021-08-17 18:24:41 +0300

    PreRelease v0.1.0

--- MyNotesApi/DataContext/DataContext.cs
+++ MyNotesApi/DataContext/DataContext.cs
@@ -15,7 +15,7 @@ namespace MyNotesApi.DataContext
 
         public MyDataContext(DbContextOptions<MyDataContext> options) : base(options)
         {
-            if (Database.EnsureCreated()) Database.Migrate();
+            Database.Migrate();
             this.ChangeTracker.LazyLoadingEnabled = false;
         }
         private readonly DatabaseSettings dbSettings;

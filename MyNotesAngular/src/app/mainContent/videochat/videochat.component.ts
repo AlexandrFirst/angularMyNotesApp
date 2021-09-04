c3f6e0798eaf530a64ade2e62548bb188dff04bb
Olexandr Lohvinov <ato31ato@gmail.com>
2021-09-04 15:38:03 +0300

    some animations added

--- MyNotesAngular/src/app/mainContent/videochat/videochat.component.ts
+++ MyNotesAngular/src/app/mainContent/videochat/videochat.component.ts
@@ -11,34 +11,19 @@ import { add, less } from './controls/script.js';
 })
 export class VideochatComponent implements OnInit {
 
+  isVideoChatActive = true;
+  isVideoOn = true;
+  isAudio = true;
+
   constructor() { }
 
   ngOnInit() {
-
-
-    let Body = document.body;
-    let Add = document.createElement('div');
-    Add.className = 'more';
-    Add.addEventListener("click", function (event) {
-      add();
-    });
-
-    let Less = document.createElement('div');
-    Less.className = 'less';
-    Less.addEventListener("click", function (event) {
-      less();
-    });
-
-
-    Body.appendChild(Add);
-    Body.appendChild(Less);
-
-
-
-
     Dish();
     window.onresize = Dish;
+  }
 
+  ToggleVideoChat() {
+    this.isVideoChatActive = !this.isVideoChatActive;
   }
 
 }
